84e83f4a9b166190f1151d65b66638c044d5eeb7
Jins-Augustine <145307894+Jins-Augustine@users.noreply.github.com>
2024-10-17 10:52:43 +0530

    Update BinaryTreeArray.c

--- Data Structure/BinaryTreeArray.c
+++ Data Structure/BinaryTreeArray.c
@@ -1,59 +1,45 @@
 #include<stdio.h>
-
-void buildTree(int a[], int i, int item)
+void buildTree(int a[],int i,int item)
 {
-    a[i] = item;
-    int val;
-    char ch;
-
-    // Insert left child
-    printf("Do you want to insert a left child of %d (y/n): ", item);
-    scanf(" %c", &ch);  // Space before %c to ignore newline
-    if(ch == 'y' || ch == 'Y')
-    {
-        printf("Enter the left child of %d: ", item);
-        scanf("%d", &val);
-        buildTree(a, (2*i) + 1, val);
-    }
-
-    // Insert right child
-    printf("Do you want to insert a right child of %d (y/n): ", item);
-    scanf(" %c", &ch);  // Space before %c to ignore newline
-    if(ch == 'y' || ch == 'Y')
-    {
-        printf("Enter the right child of %d: ", item);
-        scanf("%d", &val);
-        buildTree(a, (2*i) + 2, val);
-    }
+	a[i]=item;
+	int val;
+	char ch;
+	printf("Do you want to insert a left child for %d(y/n):",item);
+	scanf(" %c",&ch);
+	if(ch=='y'||ch=='Y')
+	{
+		printf("Enter the left child of %d:",item);
+		scanf("%d",&val);
+		buildTree(a,(2*i)+1,val);
+	}
+	printf("Do you want to insert a right child for %d",item);
+	scanf(" %c",&ch);
+	if(ch=='y' || ch=='Y')
+	{
+		printf("Enter the right child of %d:",item);
+		scanf("%d",&val);
+		buildTree(a,(2*i)+2,val);
+	}
 }
-
-void main()
+	
+int main()
 {
-    int a[100], item, i;
-    
-    // Initialize the array
-    for(i = 0; i < 100; i++)
-        a[i] = -1;
-    
-    // Input the root node
-    printf("Enter the root node: ");
-    scanf("%d", &item);
-    
-    // Build the tree starting from the root
-    buildTree(a, 0, item);
-    
-    // Display the binary tree
-    printf("\nTHE BINARY TREE IS:\n");
-    for(i = 0; i < 100; i++)
-    {
-        if(a[i] == -1)
-            printf("-");  // Print dash for empty node
-        else
-            printf("%d", a[i]);  // Print actual node value
-        
-        // Add space between nodes for clarity
-        if (i < 99)
-            printf("\t");  // Use tabs for better spacing between nodes
-    }
-    printf("\n");
+	int a[100],item,n;
+	printf("Enter the total number of nodes:");
+	scanf("%d",&n);
+	for(int i=0;i<n;i++)
+		a[i]=-1;
+	printf("Enter the root node:");
+	scanf("%d",&item);
+	buildTree(a,0,item);
+	printf("\nTHE BINARY TREE IS\n");
+	for(int i=0;i<n;i++)
+	{
+		if(a[i]==-1)
+			printf("-\t");
+		else
+			printf("%d\t",a[i]);
+	}
+	return 0;
 }
+	
