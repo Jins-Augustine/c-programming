be39c2b5a2298b0dd4a5dfc32de882cc14a5f8cb
Jins-Augustine <145307894+Jins-Augustine@users.noreply.github.com>
2024-10-17 12:46:06 +0530

    Update BinaryTreeArray.c

--- Data Structure/BinaryTreeArray.c
+++ Data Structure/BinaryTreeArray.c
@@ -1,45 +1,52 @@
-#include<stdio.h>
-void buildTree(int a[],int i,int item)
-{
-	a[i]=item;
-	int val;
-	char ch;
-	printf("Do you want to insert a left child for %d(y/n):",item);
-	scanf(" %c",&ch);
-	if(ch=='y'||ch=='Y')
-	{
-		printf("Enter the left child of %d:",item);
-		scanf("%d",&val);
-		buildTree(a,(2*i)+1,val);
-	}
-	printf("Do you want to insert a right child for %d",item);
-	scanf(" %c",&ch);
-	if(ch=='y' || ch=='Y')
-	{
-		printf("Enter the right child of %d:",item);
-		scanf("%d",&val);
-		buildTree(a,(2*i)+2,val);
-	}
-}
-	
+#include <stdio.h>
 int main()
 {
-	int a[100],item,n;
-	printf("Enter the total number of nodes:");
-	scanf("%d",&n);
-	for(int i=0;i<n;i++)
-		a[i]=-1;
-	printf("Enter the root node:");
-	scanf("%d",&item);
-	buildTree(a,0,item);
-	printf("\nTHE BINARY TREE IS\n");
-	for(int i=0;i<n;i++)
-	{
-		if(a[i]==-1)
-			printf("-\t");
-		else
-			printf("%d\t",a[i]);
-	}
-	return 0;
+    int limit, i, pos;
+    printf("Enter the total number of nodes: ");
+    scanf("%d", &limit);
+    int A[limit];  
+    printf("Enter the node data: ");
+    for (i = 0; i < limit; i++)
+    {
+        scanf("%d", &A[i]);
+    }
+    printf("the formed array nodes\n");
+    for (i = 0; i < limit; i++)
+    {
+        printf("%d\t", A[i]);
+    }
+    printf("\nEnter the position you want to check: ");
+    scanf("%d", &pos);
+    printf("\nthe element at position %d is...%d\n",pos,A[pos]);
+    if (pos < 0 || pos >= limit)
+    {
+        printf("Invalid position\n");
+        return 1;  
+    }
+    if (pos > 0)
+    {
+        printf("Parent of %d is: %d\n", A[pos], A[(pos-1) / 2]);
+    } else
+    {
+        printf("No parent\n");
+    }
+
+   
+    if ((2 * pos)+1 >= limit)
+    {
+        printf("No left child\n");
+    }
+    else
+    {
+        printf("Left child of %d is: %d\n", A[pos], A[(2 * pos)+1]);
+    }
+    if ((2 * pos )+ 2 >= limit)
+    {
+        printf("No right child\n");
+    } else
+    {
+        printf("Right child of %d is: %d\n", A[pos], A[(2 * pos )+ 2]);
+    }
+
+    return 0;  
 }
-	
